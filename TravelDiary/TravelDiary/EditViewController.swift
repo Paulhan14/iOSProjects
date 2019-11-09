6169a9f3a6e5c141c6048d537f1d9879524c13e9
Jiaxing Han <juh379@psu.edu>
2019-11-09 17:22:24 -0500

    Added final project

---
+++ TravelDiary/TravelDiary/EditViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EditViewController.swift
+//  TravelDiary
+//
+//  Created by Jiaxing Han on 11/9/19.
+//  Copyright © 2019 Jiaxing Han. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet weak var textEditView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TravelDiary/TravelDiary/MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  TravelDiary
+//
+//  Created by Jiaxing Han on 11/9/19.
+//  Copyright © 2019 Jiaxing Han. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
