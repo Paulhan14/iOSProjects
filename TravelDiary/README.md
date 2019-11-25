41666265096656e9c846e61e6e66a3a467c5045a
Jiaxing Han <juh379@psu.edu>
2019-11-24 22:26:22 -0500

    Added readme and calendar

---
+++ TravelDiary/README.md
@@ -0,0 +1,5 @@
+1. Open terminal
+2. Change directory to the project's folder
+3. Run pod install
+
+When open the project, please open TravelDiary.xcworkspace.
--- TravelDiary/TravelDiary/CalendarViewController.swift
+++ TravelDiary/TravelDiary/CalendarViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class CalendarViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+class CalendarViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var nextMonthButton: DesignableButton!
     @IBOutlet weak var backMonthButton: DesignableButton!
+    @IBOutlet weak var monthYearLabel: UILabel!
     @IBOutlet weak var dateView: UICollectionView!
+    // MARK: - Detail view
+    @IBOutlet weak var detailView: DesignableView!
+    @IBOutlet weak var stepLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageWidth: NSLayoutConstraint!
+    
+    let postController = PostController.postController
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var displayedYear = Calendar.current.component(.year, from: Date())
+    var displayedMonth = Calendar.current.component(.month, from: Date())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
         dateView.dataSource = self
         dateView.delegate = self
-        // Do any additional setup after loading the view.
+        setupCalendar()
+        detailView.isHidden = true
     }
     
 
@@ -38,17 +53,119 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource, UICo
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 31
+        return self.numberOfDays() + self.getWeekDay() - 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell =
             collectionView.dequeueReusableCell(withReuseIdentifier:
-                Constant.CellIdentifier.dateCell, for: indexPath)
-        if let textLabel = cell.contentView.subviews[0] as? UILabel{
-            textLabel.text = "\(indexPath.row)"
+                Constant.CellIdentifier.dateCell, for: indexPath) as! DateCollectionViewCell
+        let weekDayOf1st = self.getWeekDay()
+        if indexPath.row < (weekDayOf1st - 1) {
+            cell.dateLabel.text = ""
+        } else {
+            cell.dateLabel.text = "\(indexPath.row + 2 - weekDayOf1st)"
         }
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        for post in postController.posts {
+            if let postDate = post.time {
+                let format = DateFormatter()
+                format.dateFormat = "MM/dd/yyyy"
+                let postDateString = format.string(from: postDate)
+                if postDateString == self.getSelectedDate(indexPath) {
+                    populateViewWith(post)
+                    break
+                } else {
+                    detailView.isHidden = true
+                }
+            }
+        }
+    }
+    
+    // MARK: - Buttons
+    @IBAction func backPressed(_ sender: Any) {
+        displayedMonth -= 1
+        if displayedMonth == 0 {
+            displayedYear -= 1
+            displayedMonth = 12
+        }
+        setupCalendar()
+    }
+    
+    @IBAction func nextPressed(_ sender: Any) {
+        displayedMonth += 1
+        if displayedMonth == 13 {
+            displayedYear += 1
+            displayedMonth = 1
+        }
+        setupCalendar()
+    }
+    
+    
+    
+    // MARK: - Helpers
+    func setupCalendar() {
+        let displayText = months[displayedMonth - 1] + " \(displayedYear)"
+        monthYearLabel.text = displayText
+        self.dateView.reloadData()
+    }
+    
+    func numberOfDays() -> Int {
+        let dateComponents = DateComponents(year: displayedYear, month: displayedMonth)
+        let date = Calendar.current.date(from: dateComponents)!
+        let range = Calendar.current.range(of: .day, in: .month, for: date)
+        return range?.count ?? 0
+    }
+    
+    func getWeekDay() -> Int {
+        let dateComponents = DateComponents(year: displayedYear, month: displayedMonth)
+        let date = Calendar.current.date(from: dateComponents)!
+        return Calendar.current.component(.weekday, from: date)
+    }
+    
+    func getSelectedDate(_ indexPath: IndexPath) -> String {
+        let dateToday = indexPath.row - 4
+        let dateString = "\(displayedMonth)/\(dateToday)/\(displayedYear)"
+        return dateString
+    }
+    
+    func populateViewWith(_ post: Post) {
+        detailView.isHidden = false
+        textField.text = post.text ?? ""
+        if let weather = post.weather {
+            weatherImage.image = UIImage(named: weather)
+            weatherLabel.text = weather
+        }
+        if let steps = post.steps {
+            stepLabel.text = steps
+        }
+        if let imageData = post.image {
+            if imageData.description == "0 bytes" {
+                imageWidth.constant = 0
+            } else {
+                imageWidth.constant = 134
+                imageView.image = ImageManager.shared.convertToImage(data: imageData)
+            }
+            
+        }
+    }
+}
+
+extension CalendarViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 7.0
+        let itemSize = CGSize(width: width, height: 50.0)
+        return itemSize
+    }
 }
